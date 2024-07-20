2fe204052c5c3449d9f64d4324c75a4fd9885df3
annieremeeva <annieremeeva@gmail.com>
2024-07-20 19:27:14 +0300

    display todos and handle editing

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { defineComponent } from "vue";
+import { Icon } from "@iconify/vue";
+
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+  },
+  index: {
+    type: Number,
+    required: true,
+  },
+});
+defineEmits(["toggle-complete", "edit-todo", "update-todo"]);
+</script>
+
+<template>
+  <li>
+    <input
+      type="checkbox"
+      :checked="todo.isCompleted"
+      @input="$emit('toggle-complete', index)"
+    />
+    <div>
+      <input
+        v-if="todo.isEdited"
+        class="edit-input"
+        type="text"
+        :value="todo.todo"
+        @input="$emit('update-todo', $event.target.value, index)"
+      />
+      <span :class="{ 'completed-todo': todo.isCompleted }" v-else>{{
+        todo.todo
+      }}</span>
+    </div>
+    <div class="todo-actions">
+      <Icon
+        v-if="todo.isEdited"
+        class="icon"
+        icon="ph:check-circle"
+        width="22"
+        height="22"
+        style="color: #41b080"
+        @click="$emit('edit-todo', index)"
+      />
+      <Icon
+        v-else
+        class="icon"
+        icon="ph:pencil-fill"
+        width="22"
+        height="22"
+        style="color: #41b080"
+        @click="$emit('edit-todo', index)"
+      />
+      <Icon
+        class="icon"
+        icon="ph:trash"
+        width="22"
+        height="22"
+        style="color: #f95e5e"
+      />
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+li {
+  display: grid;
+  grid-template-columns: 1fr 12fr 2fr;
+  align-items: center;
+  gap: 10px;
+  padding: 16px 10px;
+  background-color: #f1f1f1;
+  box-shadow:
+    0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+
+  &:hover {
+    .todo-actions {
+      opacity: 1;
+    }
+  }
+
+  input[type="checkbox"] {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: #fff;
+    border-radius: 50%;
+    box-shadow:
+      0 4px 6px -1px rgb(0 0 0 / 0.1),
+      0 2px 4px -2px rgb(0 0 0 / 0.1);
+
+    &:checked {
+      background-color: #41b080;
+    }
+  }
+
+  .todo-actions {
+    display: flex;
+    gap: 6px;
+    opacity: 0;
+    transition: 150ms ease-in-out;
+    .icon {
+      cursor: pointer;
+    }
+  }
+}
+
+.edit-input {
+  width: 100%;
+  padding: 2px 6px;
+  border: 2px solid #41b080;
+}
+
+.completed-todo {
+  text-decoration: line-through;
+}
+</style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
 import { ref } from "vue";
 import { uid } from "uid";
+import { Icon } from "@iconify/vue";
 const todoList = ref([]);
 
 function createTodo(todo) {
@@ -12,12 +14,39 @@ function createTodo(todo) {
     isEdited: null,
   });
 }
+
+function toggleTodoComplete(todoIndex) {
+  todoList.value[todoIndex].isCompleted =
+    !todoList.value[todoIndex].isCompleted;
+}
+
+function toggleEditTodo(todoIndex) {
+  todoList.value[todoIndex].isEdited = !todoList.value[todoIndex].isEdited;
+}
+
+function updateTodo(todoValue, todoIndex) {
+  todoList.value[todoIndex].todo = todoValue;
+}
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem
+        v-for="(todo, index) in todoList"
+        :todo="todo"
+        :index="index"
+        @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+      />
+    </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todos to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -33,6 +62,21 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
